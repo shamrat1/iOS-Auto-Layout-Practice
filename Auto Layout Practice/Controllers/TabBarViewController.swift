3c50be0327d8e74d210237a6e3b0ed47b66b13e9
Yasin Shamrat <yshamrat@gmail.com>
2019-12-23 10:45:58 +0600

    Tab Bar layout added

---
+++ Auto Layout Practice/Controllers/TabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarViewController.swift
+//  Auto Layout Practice
+//
+//  Created by Yasin Shamrat on 12/23/19.
+//  Copyright © 2019 Yasin Shamrat. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ Auto Layout Practice/Extension/UIVIewControllerExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIVIewControllerExtension.swift
+//  Auto Layout Practice
+//
+//  Created by Yasin Shamrat on 12/23/19.
+//  Copyright © 2019 Yasin Shamrat. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func changeRoot(){
+        let vc = storyboard?.instantiateViewController(withIdentifier: "TabBarViewController") as? TabBarViewController
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        appDelegate?.window?.rootViewController = vc
+        appDelegate?.window?.makeKeyAndVisible()
+    }
+}
