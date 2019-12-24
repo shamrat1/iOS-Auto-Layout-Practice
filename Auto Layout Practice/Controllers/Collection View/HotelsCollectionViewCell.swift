7f4345e856b39ee2535a5652c8bd0a92ece070da
Yasin Shamrat <yshamrat@gmail.com>
2019-12-24 16:04:49 +0600

    Hotel cell aspect not working correctly. needs attention.

---
+++ Auto Layout Practice/Controllers/Collection View/HotelsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HotelsCollectionViewCell.swift
+//  Auto Layout Practice
+//
+//  Created by Yasin Shamrat on 12/24/19.
+//  Copyright © 2019 Yasin Shamrat. All rights reserved.
+//
+
+import UIKit
+
+class HotelsCollectionViewCell: UICollectionViewCell {
+    
+}
--- Auto Layout Practice/Controllers/ExploreViewController.swift
+++ Auto Layout Practice/Controllers/ExploreViewController.swift
@@ -10,22 +10,41 @@ import UIKit
 
 class ExploreViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
    
+    @IBOutlet weak var collectionViewA: UICollectionView!
+    @IBOutlet weak var collectionViewB: UICollectionView!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        collectionViewA.delegate = self
+        collectionViewB.dataSource = self
+        
+        collectionViewB.delegate = self
+        collectionViewB.dataSource = self
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-           return 5
+        if collectionView == collectionViewA{
+            return 5
+        }else {
+            return 3
+        }
        }
        
        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionCell", for: indexPath) as! SIngleCellCollectionCollectionViewCell
         
-        return cell
+        if collectionView == collectionViewA{
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionCell", for: indexPath) as! SIngleCellCollectionCollectionViewCell
+            return cell
+            
+        }else {
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HotelsCell", for: indexPath) as! HotelsCollectionViewCell
+            return cell
+        }
+        
        }
 
 }
